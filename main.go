8529d76d71ca9c59518109940811e1f23871e345
Kento KAWAKAMI <emaxser@bonprosoft.com>
2018-11-10 20:34:04 +0900

    出力にLaTeXのテーブルを追加

--- main.go
+++ main.go
@@ -7,21 +7,17 @@ import (
 	"io"
 	"log"
 	"os"
+	"fmt"
 	"strings"
 )
 
 var (
 	inputTSV  = flag.Bool("t", false, "read tsv")
+	outputLaTeX = flag.Bool("l", false, "Output LaTeX Table")
 	delimitre = ","
 )
 
-func main() {
-	flag.Parse()
-	if *inputTSV {
-		delimitre = "\t"
-	}
-
-	fn := flag.Arg(0)
+func printCLI(fn string) {
 	f, err := os.Open(fn)
 	if err != nil {
 		log.Println(err)
@@ -47,7 +43,57 @@ func main() {
 		} else {
 			table.Append(d)
 		}
-		i += 1
+		i++
 	}
 	table.Render()
+	return
+}
+
+func printTex(fn string){
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+	defer f.Close()
+
+	i := 0
+	reader := bufio.NewReaderSize(f, 1000)
+	for {
+		lineB, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+			os.Exit(-1)
+		}
+		line := string(lineB)
+		d := strings.Split(line, delimitre)
+		if i == 0 {
+			fmt.Print("\\begin{tabular}{")
+			fmt.Print(strings.Repeat("l", len(d)))
+			fmt.Println("}")
+			fmt.Println(strings.Join(d, " & ") + "\\\\")
+			fmt.Println("\\hline")
+		} else{
+			fmt.Println(strings.Join(d, " & ") + "\\\\")
+		}
+		i++
+	}
+	fmt.Println("\\end{tabular}")
+	return
+}
+
+func main() {
+	flag.Parse()
+	if *inputTSV {
+		delimitre = "\t"
+	}
+
+	fn := flag.Arg(0)
+	if *outputLaTeX {
+		printTex(fn)
+	}else{
+		printCLI(fn)
+	}
 }
